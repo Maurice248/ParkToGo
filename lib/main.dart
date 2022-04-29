b0b685852e4f622f3bef9e5e8fa2ef7a8392d9ae
unknown <delarosauberstrike@gmail.com>
2022-04-29 19:58:21 +0800

    parkresscreen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 // import 'package:parking_app/Screens/WelcomeScreen.dart';
 import 'package:parking_app/Screens/LoginScreen.dart';
 import 'package:parking_app/Screens/RegisterScreen.dart';
+import 'package:parking_app/Screens/ParkResScreen.dart';
 
 void main() {
   runApp(MaterialApp(
