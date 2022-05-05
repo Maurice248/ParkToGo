5854071b00f126cab323a05137b089360b69069a
unknown <delarosauberstrike@gmail.com>
2022-05-05 16:28:46 +0800

    added broken timer

--- lib/Screens/ParkResScreen.dart
+++ lib/Screens/ParkResScreen.dart
@@ -1,13 +1,38 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 class ParkResScreen extends StatefulWidget {
   static var routeName = "ParkResScreen";
+  
 
   @override
   State<ParkResScreen> createState() => _ParkResScreenState();
 }
 
 class _ParkResScreenState extends State<ParkResScreen> {
+  static int maxSeconds = 60;
+  static const maxMinutes = 5;
+  int seconds = 0;
+  int minutes = maxMinutes;
+  Timer? timer; 
+
+  void startTimer() {
+    timer = Timer.periodic(const Duration(milliseconds: 1), (_){
+      if (minutes > 0) {
+        if (seconds <= 0 && minutes >= 0) {
+          setState(() => minutes--);
+          setState(() => seconds = maxSeconds);
+        }
+        setState(() => seconds--);
+      }
+    });
+    
+  }
+
+  void stopTimer(){
+    timer?.cancel();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,13 +47,26 @@ class _ParkResScreenState extends State<ParkResScreen> {
               const Image(
                 image: AssetImage('img/temp_pic_parkRes.png'),
               ),
+              buildTime(),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  startTimer();
+                },
                 child: const Text('Reserve'),
               ),
             ]
           )
         )
+    );
+  }
+
+  Widget buildTime(){
+    return Text(
+      '$minutes:$seconds',
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 80,
+      )
     );
   }
 }
